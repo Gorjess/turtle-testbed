c55bce9f544b520fe96f44188ffa2e829f665999
guard4bunny <guard4bunny@lilsheepdedabenben.local>
2022-11-13 23:27:01 +0800

    try pyplot

---
+++ polygon/matplot.py
@@ -0,0 +1,15 @@
+"""
+************ Dancing Bunny ************
+* Created by: Cap Turtle
+* DateTime: 2022/11/13 23:15 
+* Desc:
+************************************** 
+"""
+
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    x_vals = [1, 2, 3, 4, 5]
+    y_vals = [1, 4, 9, 16, 250]
+    plt.scatter(x_vals, y_vals)
+    plt.show()
